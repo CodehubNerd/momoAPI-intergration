8ac72e5abdbfbf9ede6fe94042812314c45b63ad
Culture <111436414+CodehubNerd@users.noreply.github.com>
2024-02-11 23:46:16 +0200

    momo commit

---
+++ Service/momoAPIservice.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+const uuidv4 = require('uuid').v4;
+
+const momoHost = 'sandbox.momodeveloper.mtn.com';
+const momoTokenUrl = `https://${momoHost}/collection/token`;
+const momoRequestToPayUrl = `https://${momoHost}/collection/v1_0/requesttopay`;
+
+let momoToken = null;
+
+//get token
+router.post('/get-momo-token', async (req, res) => {
+    try {
+        const { apiKey, subscriptionKey } = req.body;
+        console.log('apikey', apiKey)
+        console.log('subscriptionKey', subscriptionKey)
+
+        const momoTokenResponse = await axios.post(
+            momoTokenUrl,
+            {},
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Ocp-Apim-Subscription-Key': subscriptionKey,
+                    Authorization: `Basic ${apiKey}`,
+                },
+            }
+        )
+        console.log('data from mobile money', momoTokenResponse.data)
+        momoToken = momoTokenResponse.data.access_token;
+    } catch (error) {
+        console.error('error from mobile money', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+//request to pay 
+router.post('/request-to-pay', async (req, res) => {
+    try {
+        if (!momoToken) {
+            return res.status(400).json({ error: 'token not available' });
+        }
+
+        const { phoneNumber, totalAmount } = req.body;
+
+        // Generate a UUID
+        const referenceId = uuidv4();
+
+        const body = {
+            amount: totalAmount,
+            currency: 'SZL',
+            externalId: '1532',
+            payer: {
+                partyIdtype: 'MSISDN',
+                partyId: phoneNumber,
+            },
+            payerMessage: 'Payment for DSTV',
+            payeeNote: 'Your DSTV monthly subscriptions',
+        };
+
+        const momoResponse = await axios.post(
+            momoRequestToPayUrl,
+            body,
+            {},
+            {
+                headers: {
+                    'X-Reference-Id': referenceId,
+                    'X-Target-Enviroment': 'sandbox',
+                    Authorization: `Bearer ${momoToken}`,
+                    'Content-type': 'application/json',
+                    'Cache-Control': 'no-cache',
+                },
+            }
+        );
+        res.json({ momoResponse: momoResponse.data });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,59 @@
+
+const express = require('express');
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require('cors');
+
+
+const momoservice = require('./Service/momoAPIservice.js');
+
+const app = express();
+require('dotenv').config();
+const port = 5000;
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+
+
+const allowedOrigins = [
+    'http://127.0.0.1:5500',
+
+];
+
+
+app.use(cors({
+    origin: function (origin, callback) {
+        console.log('origin:', origin);
+        console.log('allowedOrigins:', allowedOrigins);
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
+
+const apiKey = process.env.OCP_APIM_KEY;
+const subscriptionKey = process.env.API_KEY;
+
+app.get('/api/keys', (req, res) => {
+    res.json({
+        apiKey: apiKey,
+        subscriptionKey: subscriptionKey,
+    });
+});
+
+
+app.use("/api", momoservice);
+
+app.get('/', (req, res) => {
+    res.send('API running');
+});
+
+app.listen(port, () => {
+    console.log(`server running at port ${port}`);
+});
